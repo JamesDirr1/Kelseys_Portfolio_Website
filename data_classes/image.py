6873eb537459fdd6ce317744a7f312185dafc820
JamesDirr1 <dirrj2@mymail.nku.edu>
2025-02-01 18:48:29 -0500

    update project and image data class

--- data_classes/image.py
+++ data_classes/image.py
@@ -9,6 +9,6 @@ class image:
     image_id: Optional[int] = None
     image_title: Optional[str] = None
     image_desc: Optional[str] = None
-    image_url: str = "https://static.wixstatic.com/media/0fee66_18f00ad0221142dfbd4c21f7e54d425f~mv2.png/v1/fill/w_549,h_289,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/Kelsey_Spade_Header.png"
+    image_url: str
     image_weight: int
     project_id: int
--- data_classes/project.py
+++ data_classes/project.py
@@ -1,15 +1,16 @@
-import data_classes
+from dataclasses import dataclass
 from typing import Optional
 from datetime import date
 
 #Data class that represents the columns from the project table.
 
 
-@data_classes
+@dataclass
 class project:
-    project_id: Optional[int] = None
     project_title: str
     project_date: date = date.today()
-    project_desc: Optional[str] = None
+    project_image_url: str = "https://static.wixstatic.com/media/0fee66_18f00ad0221142dfbd4c21f7e54d425f~mv2.png/v1/fill/w_549,h_289,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/Kelsey_Spade_Header.png"
     project_image_id: Optional[int] = None
+    project_id: Optional[int] = None
+    project_desc: Optional[str] = None
     category_id: Optional[int] = None
